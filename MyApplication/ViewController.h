71e55354047840325523c98d69000288652d21a9
DwightDing <cndcg1994@hotmail.com>
2016-06-04 09:11:55 +0800

    Initial Commit

---
+++ MyApplication/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyApplication
+//
+//  Created by DwightDing on 16/6/4.
+//  Copyright © 2016年 Ding Chenguang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyApplication/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MyApplication
+//
+//  Created by DwightDing on 16/6/4.
+//  Copyright © 2016年 Ding Chenguang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
